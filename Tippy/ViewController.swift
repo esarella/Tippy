5181f20a8a6746b11418b79b450d660f702677d9
Emmanuel Sarella <emmanuel.r.sarella@questdiagnostics.com>
2017-03-11 12:16:21 -0800

    Mvp
    Basic app reeady

--- Tippy/ViewController.swift
+++ Tippy/ViewController.swift
@@ -10,9 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        billField.becomeFirstResponder()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +26,43 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func onTap(_ sender: Any) {
+        self.view.endEditing(true)
+    }
+    
+    @IBAction func calculateTip(_ sender: AnyObject) {
 
+        let tipPercentage = [0.18, 0.2, 0.25]
+        
+        let bill = Double(billField.text!) ?? 0
+        let tip = bill * tipPercentage[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+        
+        
+    }
+    
+    func getUserDefaults() -> UserDefaults
+    {
+        let defaults = UserDefaults.standard
+        
+        let timeStamp = defaults.integer(forKey: "TimeStamp")
+        let billAmount = defaults.integer(forKey: "BillAmount")
+        let tipControlSegmentIndex = defaults.integer(forKey: "TipControlSegmentIndex")
+        
+        return defaults
+    }
+
+    func setUserDefaults()
+    {
+        let defaults = UserDefaults.standard
+        defaults.set(25, forKey: "TimeStamp")
+        defaults.set(25, forKey: "BillAmount")
+        defaults.set(1, forKey: "TipControlSegmentIndex")
+
+        defaults.synchronize()
+    }
 }
 
